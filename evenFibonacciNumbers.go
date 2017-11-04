055030cdf3fcae0d2c400a585b49c91bc55aa5c3
behouba <behouba@gmail.com>
2017-11-05 00:06:48 +0300

    Even Fibonacci numbers challenge solved

--- evenFibonacciNumbers.go
+++ evenFibonacciNumbers.go
@@ -2,20 +2,34 @@ package main
 
 import "fmt"
 
-func allFibonacciNumbersTo(num int) []int {
-	slice := make([]int, 1, 10)
-	slice[0] = 1
-	slice[1] = 2
-	x := 0
-	for x < num {
-		y := slice[-1] + slice[-2]
-		slice = append(slice, y)
-		x = slice[len(slice)-1]
+// function to find all fibonacci number to a defined value and return all fibonacci numbers in an int slice
+func allFibonacciNumbersTo(maxValue int) []int {
+	fibonacciSlice := []int{1, 2}
+	i := fibonacciSlice[1]
+	for i < maxValue {
+		newFib := fibonacciSlice[len(fibonacciSlice)-1] + fibonacciSlice[len(fibonacciSlice)-2]
+		if newFib <= maxValue {
+			fibonacciSlice = append(fibonacciSlice, newFib)
+		}
+		i = newFib
 	}
-	return slice
+	return fibonacciSlice
+}
+
+// function that make addition of all the even elements of a slice and return that sum
+func sumPairsValue(slice []int) int {
+	var sum int
+	for _, x := range slice {
+		if x%2 == 0 {
+			sum += x
+		}
+	}
+	return sum
 }
 
 func main() {
-
-	fmt.Println(allFibonacciNumbersTo(40))
+	// functions are tested by the number 4 million
+	fiboSlice := allFibonacciNumbersTo(4000000)
+	evenValueSum := sumPairsValue(fiboSlice)
+	fmt.Println(evenValueSum)
 }
